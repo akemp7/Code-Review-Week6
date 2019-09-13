f1b3d99194fea639bbc287520eb5024396d47c34
Anita Kemp <anita.k.kemp@gmail.com>
2019-09-13 16:22:01 -0700

    Add null results as if statement to promise function

--- src/main.js
+++ src/main.js
@@ -13,32 +13,38 @@ $(document).ready(function(event){
     let promiseName = treatment.getDoctorName(doctorName);
 
     promiseIssue.then(function(response){
-      let body = JSON.parse(response);
-      $('.results').append(`${body["data"][0]["practices"][0]["name"]}`);
-      $('.results').append(`${body["data"][0]["practices"][0]["accepts_new_patients"]}`);
-      $('.results').append(`${body["data"][0]["practices"][0]["visit_address"]["street"]}`);
-      $('.results').append(`${body["data"][0]["practices"][0]["visit_address"]["street2"]}`);
-      $('.results').append(`${body["data"][0]["practices"][0]["visit_address"]["zip"]}`);
-      $('.results').append(`${body["data"][0]["practices"][0]["phones"][0]["number"]}`);
+    let body = JSON.parse(response);
+      if (`${body["data"][0]}` === ""){
+        return $('.results').text("No doctors meet your criteria");
+      } else {
+        $('.results').append(`${body["data"][0]["practices"][0]["name"]}`);
+        $('.results').append(`${body["data"][0]["practices"][0]["accepts_new_patients"]}`);
+        $('.results').append(`${body["data"][0]["practices"][0]["visit_address"]["street"]}`);
+        $('.results').append(`${body["data"][0]["practices"][0]["visit_address"]["street2"]}`);
+        $('.results').append(`${body["data"][0]["practices"][0]["visit_address"]["zip"]}`);
+        $('.results').append(`${body["data"][0]["practices"][0]["phones"][0]["number"]}`);
+      }
     }, function(error) {
       $('.error').append(`There was an error processing your request: ${error.message}`)
-    }); 
+  });
 
 
-    if (`${body["data"][0]}` === '[]'){
-      return $('.results').text("No results found");
-  }
+
 
     promiseName.then(function(response){
-      let body = JSON.parse(response);
-      $('.results').append(`${body["data"][0]["practices"][0]["name"]}`);
-      $('.results').append(`${body["data"][0]["practices"][0]["accepts_new_patients"]}`);
-      $('.results').append(`${body["data"][0]["practices"][0]["visit_address"]["street"]}`);
-      $('.results').append(`${body["data"][0]["practices"][0]["visit_address"]["street2"]}`);
-      $('.results').append(`${body["data"][0]["practices"][0]["visit_address"]["zip"]}`);
-      $('.results').append(`${body["data"][0]["practices"][0]["phones"][0]["number"]}`);
+    let body = JSON.parse(response);
+      if (`${body["data"][0]}` === ""){
+        return $('.results').text("No doctors meet your criteria");
+      } else {
+        $('.results').append(`${body["data"][0]["practices"][0]["name"]}`);
+        $('.results').append(`${body["data"][0]["practices"][0]["accepts_new_patients"]}`);
+        $('.results').append(`${body["data"][0]["practices"][0]["visit_address"]["street"]}`);
+        $('.results').append(`${body["data"][0]["practices"][0]["visit_address"]["street2"]}`);
+        $('.results').append(`${body["data"][0]["practices"][0]["visit_address"]["zip"]}`);
+        $('.results').append(`${body["data"][0]["practices"][0]["phones"][0]["number"]}`);
+      }
     }, function(error) {
-      $('.error').append(`There was an error processing your request:${error.message}`)
+      $('.error').append(`There was an error processing your request: ${error.message}`)
     });
   });
 });
