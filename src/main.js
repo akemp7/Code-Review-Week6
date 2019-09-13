0ab9a6dbcd9e5178ca672f75c750c10a05a2ae0a
Anita Kemp <anita.k.kemp@gmail.com>
2019-09-13 10:16:21 -0700

    Add UI and Business logic to yield doctor by medical issue

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import {treatmentSearch} from './scripts';
 import 'bootstrap';
 import $ from 'jquery';
 import './styles.css'
@@ -6,6 +7,13 @@ $(document).ready(function(event){
   $('#medIssue').submit(function(event){
     event.preventDefault();
     let medicalIssue = $('input[name=medicalIssue]').val();
-    let doctor = $('input[name=doctor]').val();
+    let treatment = new treatmentSearch();
+    let promise = treatment.getTreatment(medicalIssue);
+
+
+    promise.then(function(response){
+      let body = JSON.parse(response);
+      console.log(`${body.data.practices.name}`);
+    });
   });
 });
--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,17 @@
-export function medIssue(issue){
-  const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=true&first_name=true&last_name=true&query=true&specialty_uid=true&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&sort=best-match-desc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`
+export class treatmentSearch{
+  getTreatment(issue){
+    return new Promise(function(resolve, reject){
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query?&location=or-portland&sort=best-match-desc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`
+      request.onload = function(){
+        if(this.status === 200){
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
 }
